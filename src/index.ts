1ce1c5e3ee0d92a3fe68d11beb2a29c4542d800d
kevoff <suviacruzfer@gmail.com>
2023-09-19 10:54:14 -0300

    cors habilitados

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }))
+
+app.use(cors())
 
 app.use(express.json())
 
