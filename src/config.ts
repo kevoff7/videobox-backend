ad1ae669a90be068f2013ffed1f22a2849136234
kevoff <suviacruzfer@gmail.com>
2023-09-18 23:29:28 -0300

    render connect

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ import 'dotenv/config'
 export const db = { port: process.env.portDataBase, user: process.env.user, password: process.env.password, host: process.env.host, database: process.env.database }
 export const server = { port: process.env.PORT }
 export const jwtEnv = { secret_seed: process.env.SECRET_JWT_SEED }
-export const dbprod = { renderUrl: process.env.RENDER_URL }
+export const dbprod = { renderUrl: process.env.DBConnLink }
--- src/db.ts
+++ src/db.ts
@@ -3,13 +3,15 @@ import { dbprod } from './config'
 // import { db } from './config'
 
 // const PORT = db.port ?? 5432
-
 export const pool = new Pool({
   // user: db.user,
   // password: db.password,
   // host: db.host,
   // port: Number(PORT),
   // database: db.database
-  connectionString: dbprod.renderUrl
+  connectionString: dbprod.renderUrl,
+  ssl: {
+    rejectUnauthorized: false
+  }
 
 })
