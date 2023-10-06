c55b3a4d5384b05c8c7fe3e67382364744903185
kevoff <suviacruzfer@gmail.com>
2023-10-06 18:20:56 -0300

    add to vercel

--- build/database/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const config_1 = require("../config");
-const sequelize = new sequelize_1.Sequelize(`postgresql://${config_1.db.user}:${config_1.db.password}@${config_1.db.host}:${config_1.db.port}/${config_1.db.database}`, {
-    logging: false,
-    dialect: 'postgres'
-});
-exports.default = sequelize;
